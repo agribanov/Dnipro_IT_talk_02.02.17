620fe5831fd62d7cffb944a28c5fc2d338459a1d
Aleksandr Gribanov <agribanov@dataart.com>
2017-02-02 16:00:26 +0200

    SEARCH: Add input listener

--- search-app/script.js
+++ search-app/script.js
@@ -2,4 +2,8 @@ $(() => {
     const $search = $('#search');
     const $list = $('#list');
       
+    const searchChange$ =
+        Rx.Observable.fromEvent($search, 'input');
+    
+    searchChange$.subscribe((data) => console.log(data));
 })
