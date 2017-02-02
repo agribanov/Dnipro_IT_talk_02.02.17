a4aa09aa4d94a6d24b255b38871813c0b4f2ac0e
Aleksandr Gribanov <agribanov@dataart.com>
2017-02-02 16:07:20 +0200

    SEARCH: add debounce

--- search-app/script.js
+++ search-app/script.js
@@ -2,11 +2,20 @@ $(() => {
     const $search = $('#search');
     const $list = $('#list');
       
+    function getUsers(text) {
+        const request =
+            fetch('https://api.github.com/search/users?q=' + text)
+                .then(resp => resp.json());
+
+    return Rx.Observable.fromPromise(request);
+}
+
     const searchChange$ =
         Rx.Observable.fromEvent($search, 'input');
     
     searchChange$
         .map(e => e.target.value)
         .filter(text => text.length > 2)
+        .debounceTime(300)
         .subscribe((data) => console.log(data));
 })
