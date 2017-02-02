4dff3d4a6dc90d8ca43b1c71925e33e3b28dfe29
Aleksandr Gribanov <agribanov@dataart.com>
2017-02-02 15:42:59 +0200

    LEDS: add interval

--- leds-app/script.js
+++ leds-app/script.js
@@ -2,5 +2,14 @@ $(() => {
     const $greenLed = $('#greenLed');
     const $redLed = $('#redLed');
     
+    const interval$ = Rx.Observable.interval(300);
+
+    interval$.subscribe(() => {
+        $greenLed.toggleClass('on');
+    });
+    
+    interval$.subscribe(() => {
+        $redLed.toggleClass('on');
+    });
     
 })
