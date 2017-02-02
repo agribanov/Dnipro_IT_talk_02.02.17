4b71c5718c1fd43ac55beb50bf1e79411fbce4b9
Aleksandr Gribanov <agribanov@dataart.com>
2017-02-02 13:46:00 +0200

    Observable: add observer

--- observable/index.js
+++ observable/index.js
@@ -1,27 +1,27 @@
-function nextCallback(data) {
-    console.log(data);
+const observer = {
+    next: function nextCallback(data) {
+        console.log(data);
+    },
+    complete: function completeCallback() {
+        console.log('Done');
+    },
+    error: function errorCallback(err) {
+        console.log('Error: ', err);
+    }
 }
 
-function completeCallback() {
-    console.log('Done');
-}
-
-function errorCallback(err) {
-    console.log('Error: ', err);
-}
-
-function startReceivingData(next, error, complete) {
+function startReceivingData(obs) {
     let counts = 0;
     const intervalId = setInterval(() => {
         counts++;
 
-        next(counts);
-        
+        obs.next(counts);
+
         if (counts >= 5) {
             clearInterval(intervalId);
-            complete();
+            obs.complete();
         }
     }, 300);
 }
 
-startReceivingData(nextCallback, errorCallback, completeCallback);
+startReceivingData(observer);
