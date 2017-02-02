b450c4b6f5da2647a25f8f1ab2646a0687411573
Aleksandr Gribanov <agribanov@dataart.com>
2017-02-02 13:40:04 +0200

    Observable: DOM Example

--- observable/index.js
+++ observable/index.js
@@ -11,7 +11,7 @@ function errorCallback(err) {
 }
 
 function startReceivingData(next) {
-    [1, 2, 3, 4, 5].forEach(next);
+    document.addEventListener('click', next);
 }
 
 startReceivingData(nextCallback, errorCallback, completeCallback);
